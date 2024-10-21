09679db254b90086beac9203bd9b1e31f448ce4b
ankitchoubeyy <imkitchoubey@gmail.com>
2024-10-22 00:50:53 +0530

    UI bug fixed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html', // Add your HTML file here
+    './src/**/*.{html,js}', // Adjust this path as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
